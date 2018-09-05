d4c0e64d12042d7e18e16e6dd338f9efbe9413f2
Ketomaa <tketomaa5@gmail.com>
2018-09-05 13:56:21 +0300

    HelloWorld project initialized

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello And Welcome");
+            Console.WriteLine("Tatu");
+            Console.WriteLine("Ketomaa");
+            Console.WriteLine("You Have Mail!");
+        }
+    }
+}
